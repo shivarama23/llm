654b56b2c3d9184939c5aa0e75adfe5fdb345975
shivarama23 <shivarama23@gmail.com>
2023-07-12 13:36:56 +0530

    Update llm_train.py

--- llm_train.py
+++ llm_train.py
@@ -1,2 +1,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+
+model_name = "EleutherAI/gpt-neox-20b"
+#Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
