7767c58c7357fc60708ffd22d850c445dbfc0b7e
shivarama23 <shivarama23@gmail.com>
2023-07-12 13:49:00 +0530

    Update llm_train.py

--- llm_train.py
+++ llm_train.py
@@ -5,3 +5,34 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 model_name = "EleutherAI/gpt-neox-20b"
 #Tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=quant_config, device_map={"":0})
+
+model.gradient_checkpointing_enable()
+
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
+
+model = prepare_model_for_kbit_training(model)
+
+config = LoraConfig(
+    r=8, 
+    lora_alpha=32, 
+    target_modules=["query_key_value"], 
+    lora_dropout=0.05, 
+    bias="none", 
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, config)
+
+##%
+from datasets import load_dataset
+data = load_dataset("Abirate/english_quotes")
+data = data.map(lambda samples: tokenizer(samples["quote"]), batched=True)
