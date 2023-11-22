15985e745ab577357f945163d1521f7c76733660
shivarama23 <shivarama23@gmail.com>
2023-11-22 13:09:43 +0530

    Update fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -8,3 +8,41 @@ streamlit_app_url = "https://your-streamlit-app-url.com"  # Replace with your St
 @app.get("/redirect_to_streamlit")
 def redirect_to_streamlit():
     return RedirectResponse(url=streamlit_app_url)
+
+
+
+#!/bin/bash
+
+# Start Streamlit app in the background
+streamlit run /app/streamlit_app.py &
+
+# Start FastAPI app
+uvicorn fastapi_app:app --host 0.0.0.0 --port 8000
+
+
+# Use a base image
+FROM python:3.9
+
+# Set working directory
+WORKDIR /app
+
+# Install necessary dependencies
+RUN pip install streamlit fastapi uvicorn
+
+# Copy your Streamlit and FastAPI files into the container
+COPY streamlit_app.py /app/streamlit_app.py
+COPY fastapi_app.py /app/fastapi_app.py
+
+# Copy the bash script
+COPY start_services.sh /app/start_services.sh
+
+# Expose ports
+EXPOSE 8501 8000
+
+# Give execute permissions to the script
+RUN chmod +x /app/start_services.sh
+
+# Start services using the script
+CMD ["/bin/bash", "/app/start_services.sh"]
+
+
