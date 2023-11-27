6fa60d398cf1f845563af2edef2881437aa099d2
shivarama23 <shivarama23@gmail.com>
2023-11-28 01:15:23 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -46,3 +46,58 @@ print("True Positives:", TP)
 print("False Positives:", FP)
 print("True Negatives:", TN)
 print("False Negatives:", FN)
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def calculate_overlap(start1, end1, start2, end2):
+    # Calculate the overlapping range
+    overlap = max(0, min(end1, end2) - max(start1, start2))
+    # Calculate the total range covered by both spans
+    total_range = max(end1, end2) - min(start1, start2)
+    # Calculate the percentage overlap
+    return (overlap / total_range) * 100
+
+def compare_entities(ground_truth, model_output):
+    metrics = {'TP': [], 'FP': [], 'TN': [], 'FN': []}
+    model_output_checked = []
+
+    for gt_entity in ground_truth:
+        match_found = False
+        for mo_entity in model_output:
+            if gt_entity[0] == mo_entity[0] and calculate_overlap(gt_entity[2], gt_entity[3], mo_entity[2], mo_entity[3]) >= 90:
+                metrics['TP'].append(gt_entity)
+                model_output_checked.append(mo_entity)
+                match_found = True
+                break
+        if not match_found:
+            metrics['FN'].append(gt_entity)
+
+    for mo_entity in model_output:
+        if mo_entity not in model_output_checked:
+            metrics['FP'].append(mo_entity)
+
+    # Assuming True Negatives are not directly identifiable in this context
+    return metrics
+
+def update_master_metrics(master_metrics, new_metrics):
+    for metric in ['TP', 'FP', 'FN']:
+        for entity in new_metrics[metric]:
+            entity_type = entity[1]
+            master_metrics[entity_type].setdefault(metric, 0)
+            master_metrics[entity_type][metric] += 1
+    return master_metrics
+
+# Example Usage
+ground_truth_list = [('Rama', 'NAME', 3, 8), ('Raipur', 'CITY', 9, 15)]
+model_output_list = [('Rama', 'NAME', 3, 8), ('Raipur', 'CITY', 9, 15), ('Hyderabad', 'CITY', 12, 21)]
+
+# Initialize master metrics dictionary
+master_metrics = {'NAME': {}, 'CITY': {}}
+
+# Compare entities
+metrics = compare_entities(ground_truth_list, model_output_list)
+print(metrics)
+# Update master metrics
+master_metrics = update_master_metrics(master_metrics, metrics)
+print(master_metrics)
+
