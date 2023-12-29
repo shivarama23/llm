67730c2dd4fd20a0aaab345d58d0c4b775c14fa0
shivarama23 <shivarama23@gmail.com>
2023-12-29 14:24:29 +0530

    Create prompt.py

---
+++ prompt.py
@@ -0,0 +1,65 @@
+from langchain.llms import OpenAI
+
+def simulate_hcp_interaction(prompt_details):
+    """
+    Simulates an interaction with a Healthcare Professional (HCP) in a pre-call scenario.
+
+    Args:
+    prompt_details (dict): A dictionary containing details for the simulation scenario.
+
+    Returns:
+    str: The response from the GPT model acting as an HCP.
+    """
+
+    # Define the prompt template
+    prompt_template = """
+    Prompt for GPT-4 as an HCP in a Pre-Call Simulator
+
+    Objective: {objective}
+
+    GPT-4 Role: {role}
+
+    Scenario Setup:
+    1. Background Information: {background_info}
+    2. Sales Pitch Context: {sales_pitch_context}
+    3. Possible Objections: {possible_objections}
+
+    Interaction Flow:
+    1. Introduction: {introduction}
+    2. Pitch Reception: {pitch_reception}
+    3. Objection Handling: {objection_handling}
+    4. Feedback and Guidance: {feedback_guidance}
+
+    Tips for GPT-4 Responses:
+    {tips}
+    """
+
+    # Populate the prompt with details
+    prompt = prompt_template.format(
+        objective=prompt_details["objective"],
+        role=prompt_details["role"],
+        background_info=prompt_details["background_info"],
+        sales_pitch_context=prompt_details["sales_pitch_context"],
+        possible_objections=prompt_details["possible_objections"],
+        introduction=prompt_details["introduction"],
+        pitch_reception=prompt_details["pitch_reception"],
+        objection_handling=prompt_details["objection_handling"],
+        feedback_guidance=prompt_details["feedback_guidance"],
+        tips=prompt_details["tips"]
+    )
+
+    # Initialize the OpenAI model
+    model = OpenAI()
+
+    # Get the response from the model
+    response = model.complete(prompt)
+
+    return response
+
+# Example usage
+prompt_details = {
+    "objective": "To simulate a realistic pre-call environment...",
+    # Add other details here...
+}
+response = simulate_hcp_interaction(prompt_details)
+print(response)
